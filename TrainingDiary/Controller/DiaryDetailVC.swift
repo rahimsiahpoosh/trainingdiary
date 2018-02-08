fd3490a64e33f1bc6e636f21cb4d7c59fe88e16f
Rahim Siahpoosh <rahimsiahpoosh@m-ipv6.cust.tele2.se>
2018-02-08 17:12:02 +0100

    detailView with functionality

--- TrainingDiary/Controller/DiaryDetailVC.swift
+++ TrainingDiary/Controller/DiaryDetailVC.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class DiaryDetailVC: UIViewController {
 
+    @IBOutlet weak var subjectLabel: UILabel!
+    
+    @IBOutlet weak var diaryTextView: UITextView!
+    
     var detailDiary:GymModel?
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let diaryDetail = detailDiary {
+            subjectLabel.text = diaryDetail.subject
+            diaryTextView.isEditable = false
+            diaryTextView.text = diaryDetail.diary
+        } else {
+            print("knas")
+        }
 
     }
 
